bf6dbeb9579c0ed114c4d4442daebde2b7e203a1
xylophonehero <nick97000@gmail.com>
2022-12-01 23:06:33 +0000

    better solution 1

--- solutions/1.ts
+++ solutions/1.ts
@@ -25,3 +25,15 @@ const result2 = file.split('\n')
 
 console.log(result)
 console.log(result2)
+
+const resultBetter = file.split('\n\n')
+  .map((elfArr) => elfArr.split('\n').reduce((acc, cur) => acc + Number(cur), 0))
+  .reduce((acc, cur) => Math.max(acc, cur), 0)
+
+const result2Better = file.split('\n\n')
+  .map((elfArr) => elfArr.split('\n').reduce((acc, cur) => acc + Number(cur), 0))
+  .reduce((acc, cur) => [Math.max(acc[0], cur), ...acc.slice(1)].sort((a, b) => a - b), [0, 0, 0])
+  .reduce((acc, cur) => acc + cur, 0)
+
+console.log(resultBetter)
+console.log(result2Better)
