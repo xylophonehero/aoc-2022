4b17c3b0cbb4dd10711a99cae7f48d582a721f1c
xylophonehero <nick97000@gmail.com>
2022-12-06 20:12:08 +0000

    6 improve one lines

--- solutions/6.ts
+++ solutions/6.ts
@@ -2,9 +2,9 @@ import readDataFile from "../utils/readFile";
 
 const data = readDataFile()
 
-const result = data.split('').findIndex((_, index, arr) => index < 3 ? false : new Set(arr.slice(index - 3, index + 1)).size === 4) + 1
+const result = data.split('').findIndex((_, i, a) => new Set(a.slice(Math.max(i - 3, 0), i + 1)).size === 4) + 1
 
-const result2 = data.split('').findIndex((_, index, arr) => index < 13 ? false : new Set(arr.slice(index - 13, index + 1)).size === 14) + 1
+const result2 = data.split('').findIndex((_, i, a) => new Set(a.slice(Math.max(i - 13, 0), i + 1)).size === 14) + 1
 
 console.log(result)
 console.log(result2)
